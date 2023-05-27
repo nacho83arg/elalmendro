ece4f832ed71ff377313f4e522ea5061b6a65c5b
Nacho <124308945+nacho83arg@users.noreply.github.com>
2023-05-27 00:55:48 -0300

    eventos y prensa

--- js/anima.js
+++ js/anima.js
@@ -2,3 +2,47 @@ document.getElementById("menu").addEventListener("click",function(){
   
     document.getElementById("navega").classList.toggle("mostrar");
   });
+
+
+  document.getElementById("lupa1").addEventListener("click", myFunction2);
+function myFunction2() {
+  let image = document.getElementById("lupa1");
+  let texto = document.getElementById("texto_s1");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+document.getElementById("lupa2").addEventListener("click", myFunction3);
+function myFunction3() {
+  let image = document.getElementById("lupa2");
+  let texto = document.getElementById("texto_s2");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa3").addEventListener("click", myFunction3);
+function myFunction3() {
+  let image = document.getElementById("lupa3");
+  let texto = document.getElementById("texto_s3");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
