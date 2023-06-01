cd014feb29c81ce68d81cf359be23cdadbb642c9
Nacho <124308945+nacho83arg@users.noreply.github.com>
2023-06-01 20:14:13 -0300

    lupas y diseño eventos

--- js/anima.js
+++ js/anima.js
@@ -4,10 +4,10 @@ document.getElementById("menu").addEventListener("click",function(){
   });
 
 
-  document.getElementById("lupa1").addEventListener("click", myFunction2);
-function myFunction2() {
-  let image = document.getElementById("lupa1");
-  let texto = document.getElementById("texto_s1");
+  document.getElementById("lupa11").addEventListener("click", myFunction11);
+function myFunction11() {
+  let image = document.getElementById("lupa11");
+  let texto = document.getElementById("texto_s11");
 
   if (image.getAttribute("src") === "icn/lupa.png") {
     image.setAttribute("src", "icn/lupa-menos.png");
@@ -19,10 +19,10 @@ function myFunction2() {
 }
 
 
-document.getElementById("lupa2").addEventListener("click", myFunction3);
-function myFunction3() {
-  let image = document.getElementById("lupa2");
-  let texto = document.getElementById("texto_s2");
+document.getElementById("lupa12").addEventListener("click", myFunction12);
+function myFunction12() {
+  let image = document.getElementById("lupa12");
+  let texto = document.getElementById("texto_s12");
 
   if (image.getAttribute("src") === "icn/lupa.png") {
     image.setAttribute("src", "icn/lupa-menos.png");
@@ -33,10 +33,132 @@ function myFunction3() {
   }
 }
 
-document.getElementById("lupa3").addEventListener("click", myFunction4);
-function myFunction4() {
-  let image = document.getElementById("lupa3");
-  let texto = document.getElementById("texto_s3");
+document.getElementById("lupa13").addEventListener("click", myFunction13);
+function myFunction13() {
+  let image = document.getElementById("lupa13");
+  let texto = document.getElementById("texto_s13");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+
+
+
+
+document.getElementById("lupa21").addEventListener("click", myFunction21);
+function myFunction21() {
+  let image = document.getElementById("lupa21");
+  let texto = document.getElementById("texto_s21");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa22").addEventListener("click", myFunction22);
+function myFunction22() {
+  let image = document.getElementById("lupa22");
+  let texto = document.getElementById("texto_s22");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa23").addEventListener("click", myFunction23);
+function myFunction23() {
+  let image = document.getElementById("lupa23");
+  let texto = document.getElementById("texto_s23");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+document.getElementById("lupa24").addEventListener("click", myFunction24);
+function myFunction24() {
+  let image = document.getElementById("lupa24");
+  let texto = document.getElementById("texto_s24");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+document.getElementById("lupa25").addEventListener("click", myFunction25);
+function myFunction25() {
+  let image = document.getElementById("lupa25");
+  let texto = document.getElementById("texto_s25");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+document.getElementById("lupa26").addEventListener("click", myFunction26);
+function myFunction26() {
+  let image = document.getElementById("lupa26");
+  let texto = document.getElementById("texto_s26");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+document.getElementById("lupa27").addEventListener("click", myFunction27);
+function myFunction27() {
+  let image = document.getElementById("lupa27");
+  let texto = document.getElementById("texto_s27");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+
+document.getElementById("lupa28").addEventListener("click", myFunction28);
+function myFunction28() {
+  let image = document.getElementById("lupa28");
+  let texto = document.getElementById("texto_s28");
 
   if (image.getAttribute("src") === "icn/lupa.png") {
     image.setAttribute("src", "icn/lupa-menos.png");
